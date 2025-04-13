9b69873b8d2a2796e4f0160a204debe108e50ac6
rytiskar <rytis.karalius321@gmail.com>
2025-04-13 19:26:39 +0300

    Add email generator endpoint to test delay

---
+++ src/main/java/lt/vu/services/EmailGenerator.java
@@ -0,0 +1,21 @@
+package lt.vu.services;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+
+@ApplicationScoped
+public class EmailGenerator implements Serializable {
+
+    public CompletableFuture<String> generateEmail() {
+
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000); // Simulating intensive task
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return "generated.email@example.com";
+        });
+    }
+}
