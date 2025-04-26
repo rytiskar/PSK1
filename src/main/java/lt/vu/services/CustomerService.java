f556b96be6550a93e79396c8f1541bcd46875eac
rytiskar <rytis.karalius321@gmail.com>
2025-04-26 22:22:39 +0300

    Remove unused code

--- src/main/java/lt/vu/services/CustomerService.java
+++ src/main/java/lt/vu/services/CustomerService.java
@@ -8,12 +8,6 @@ import lt.vu.rest.contracts.CustomerWithOrdersAndProductsDto;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
