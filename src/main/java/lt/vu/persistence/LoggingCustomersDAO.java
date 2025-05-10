afb4d5faddce016e4f9787ee53ce870c6158b92c
rytiskar <rytis.karalius321@gmail.com>
2025-05-10 12:23:41 +0300

    Add usage of CDI @Specializes

--- src/main/java/lt/vu/persistence/LoggingCustomersDAO.java
+++ src/main/java/lt/vu/persistence/LoggingCustomersDAO.java
@@ -3,24 +3,29 @@ package lt.vu.persistence;
 import lt.vu.entities.Customer;
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Specializes;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import java.util.List;
 
 @RequestScoped
-public class CustomersDAO {
+@Specializes
+public class LoggingCustomersDAO extends CustomersDAO {
     @Inject
     private EntityManager em;
 
     public void persist(Customer customer){
+        System.out.println("[LoggingCustomersDAO] persisting Customer with id: " + customer.getId());
         this.em.persist(customer);
     }
 
     public Customer findOne(Long id){
+        System.out.println("[LoggingCustomersDAO] looking for customer with id: " + id);
         return em.find(Customer.class, id);
     }
 
     public List<Customer> findAll() {
+        System.out.println("[LoggingCustomersDAO] retrieving all customers");
         return em.createQuery("SELECT DISTINCT c FROM Customer c", Customer.class)
                 .getResultList();
     }
